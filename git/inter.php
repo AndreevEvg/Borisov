1010e87733526da6e60a82c8862d8510a2fdd622
Evgeniy Andreev <e.andreev@yandex.ru>
2017-11-20 20:46:38 +0300

    -

--- git/inter.php
+++ git/inter.php
@@ -2,17 +2,3 @@
 
 echo "Hello Evgeniy<br>";
 echo "Hello Ekaterina<br>";
-echo "Hello Irina<br>";
-for ($i = 0; $i < 10; $i++) {
-    echo $i . "<br>";
-}
-
-for ($i = 0; $i < 5; $i++) {
-    echo $i . "<br>";
-}
-
-
-echo "LN - 1";
-echo "LN - 3";
-echo "LN - 2";
-echo "LN - 4";
